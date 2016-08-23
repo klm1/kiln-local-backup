f70c604344804ad157cfe80d8624dc208f9dfccd
Ken Morse <ken@iliumsoft.com>
2016-08-23 16:35:53 -0400

    Update script to backup both Hg and Git repos, can also now use SSH.

---
+++ README.md
@@ -0,0 +1,87 @@
+﻿# OVERVIEW
+
+This is a backup script to maintain a copy of all of your Kiln Mercurial and Git 
+repositories on your local system.
+
+## GETTING STARTED
+
+For Windows you need:
+
+* A Kiln account
+* A FogBugz API token
+* Python 2.4 or higher
+* simplejson (not needed with Python 2.6 or higher)
+* The Kiln Tools installed
+
+On other platforms, you’ll also need to install:
+
+* Mecurial 1.3 or higher (if you have Hg repos in Kiln)
+* Git 2.9 or higher (if you have Git repos in Kiln)
+* The KilnAuth Mercurial extension
+
+If you are missing any of these, see **REFERENCES** at the end of this document.
+
+## BACKING UP
+
+All repositories are backed up to the directory you specify. For example, if
+you want to back up to C:\KilnBackups, run:
+
+`python kiln-local-backup.py C:\KilnBackups`
+
+You will be prompted for your API token and Kiln server name. If your 
+credentials aren’t stored in KilnAuth, you’ll also be prompted for your 
+password.
+
+Your API token and server name are saved to a file in the backup directory,
+so you won’t have to enter them next time.
+
+Once KilnAuth has your credentials, you will not be prompted for your
+password again.
+
+To see the full syntax, including options for passing your API token and
+server name on the command line, type:
+
+`python kiln-local-backup.py --help`
+
+## FAQS
+
+Q: My backup is empty!
+
+A: No, it’s not. With Mercurial and Git, the entire repository is stored 
+in the .hg and .git subirectories respectively, which may be hidden.
+You can clone the repository to see that the files are really there, 
+if it makes you feel better.
+
+Alternately, you can specify the `--update` command-line option. Then you’ll
+see your working files. The working files that you see are just the latest
+version of your project, but rest assured the script is backing up your
+whole repository, including all history. That’s located in the .hg
+subdirectory for Mercurial, and the .git directory for Git.
+
+
+Q: Scheduled backups don’t work.
+
+A: Make sure you are running the backup under THE SAME USER ACCOUNT you were
+using when you entered your password for KilnAuth.
+
+
+Q: What platforms does this run on?
+
+A: I have tested it on Mac OS X 10.1, Windows Server 2008 R2, and Windows 10.
+
+
+Q: Can I back up repositories with non-ASCII names?
+
+A: It should work. On Windows, non-ASCII characters in the repository name
+will be replaced with equivalent XML character references. On Unix/Mac,
+non-ASCII names will be used as-is.
+
+REFERENCES
+
+To get a FogBugz API token, see: http://goo.gl/buFUPB
+
+To get the KilnAuth Mercurial extension, install the Kiln Client, or see your
+Kiln Tools page. The URL is: http://[your kiln site]/Tools.
+
+simplejson is only needed for Python versions earlier than 2.6. It can be
+found at http://goo.gl/nv3tu.
--- kiln_local_backup.py
+++ kiln_local_backup.py
@@ -3,7 +3,7 @@
 """
 kiln_local_backup.py
 
-Copyright (c) 2010 Nate Silva
+Copyright (c) 2010-2016 Nate Silva, Ken Morse
 
 Permission is hereby granted, free of charge, to any person
 obtaining a copy of this software and associated documentation files
@@ -25,7 +25,7 @@ ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 import sys
 import os
@@ -62,20 +62,21 @@ def parse_command_line(args):
     global debug
 
     usage = 'usage: %prog [options] DESTINATION-DIR'
-    description = 'Backs up all available Mercurial repositories on Kiln ' + \
-        'by cloning them (if they have not been backed up before), or by ' + \
-        'pulling changes. In order to run this without user interaction, ' + \
-        'you must install the FogBugz "KilnAuth" Mercurial extension and ' + \
-        'clone at least one repository so that your credentials are saved.'
+    description = 'Backs up all available Mercurial and Git repositories on Kiln ' + \
+        'by cloning them (if they have not been backed up before), or by pulling ' + \
+        'changes. In order to run this without user interaction, you must install ' + \
+        'the FogBugz "KilnAuth" Mercurial extension and appropriate Git extension ' + \
+        'then clone at least one repository so that your credentials are saved.'
     version = "%prog, v" + __version__
 
     parser = OptionParser(usage=usage, description=description, version=version)
     parser.formatter = IndentedHelpFormatter(max_help_position=30)
 
     parser.add_option('-t', '--token', dest='token', help='FogBugz API token')
-    parser.add_option('-s', '--server', dest='server', help='Kiln server name')
-    parser.add_option('--scheme', dest='scheme', type='choice',
-        choices=('https', 'http'), help='scheme used to connect to server')
+    parser.add_option('-s', '--server', dest='server', help='Kiln server name, e.g.: ' + \
+        'https://name.kilnhg.com')
+    parser.add_option('--ssh', dest='ssh', action='store_true',
+        default=False, help='Use SSH when connecting to repos')
     parser.add_option('-q', '--quiet', dest='verbose', action='store_false',
         default=True, help='non-verbose output')
     parser.add_option('-d', '--debug', dest='debug', action='store_true',
@@ -111,17 +112,10 @@ def parse_command_line(args):
         if not options.server and 'server' in config_data:
             options.server = config_data['server']
 
-        if not options.scheme and 'scheme' in config_data:
-            options.scheme = config_data['scheme']
-
-    # default to https if still no scheme specified
-    if not options.scheme:
-        options.scheme = 'https'
-        
     return (options, destination_dir)
 
 
-def get_repos(scheme, server, kiln_path, token, verbose):
+def get_repos(server, token, ssh, verbose):
     """
     Query Kiln to get the list of available repositories. Return the
     list, sorted by the Kiln “full name” of each repository.
@@ -130,7 +124,7 @@ def get_repos(scheme, server, kiln_path, token, verbose):
     if verbose:
         print console_encode('Getting the list of repositories from %s' % server)
     
-    url = '%s://%s/%sApi/2.0/Project/?token=%s' % (scheme, server, kiln_path, token)
+    url = '%s/Api/2.0/Project/?token=%s' % (server, token)
     if debug:
         print "get Projects: url:", url
     projects = json.load(urllib2.urlopen(url))
@@ -138,6 +132,8 @@ def get_repos(scheme, server, kiln_path, token, verbose):
         sys.exit('Error from server: ' + projects['errors'][0]['sError'])
 
     ourRepos = []
+    numHgRepos = 0
+    numGitRepos = 0
 
     for project in projects:
         for repoGroup in project['repoGroups']:
@@ -147,22 +143,41 @@ def get_repos(scheme, server, kiln_path, token, verbose):
             if repoGroup['sName'] == '':
                 repoGroup['sName'] = 'Group'
             for repo in repoGroup['repos']:
-                # we'll build a URL using the slugs but also save the human-readable
-                # names for sorting our list by project, group, repo
-                repoPath = project['sSlug'] + '/' + repoGroup['sSlug'] + '/' + repo['sSlug']
+                # we'll get the appropriate full URL but also save the human-readable
+                # names for sorting our list by VCS type, project, group, repo
+                # Per https://developers.fogbugz.com/default.asp?W166:
+                # VCS=1 is Mercurial (Hg), VCS=2 is Git
+
+                localPath = project['sSlug'] + '/' + repoGroup['sSlug'] + '/' + repo['sSlug']
+
+                if repo['vcs'] == 1:
+                    numHgRepos += 1
+                    if ssh:
+                        repoPath = repo['sHgSshUrl']
+                    else:
+                        repoPath = repo['sHgUrl']
+                elif repo['vcs'] == 2:
+                    numGitRepos += 1
+                    if ssh:
+                        repoPath = repo['sGitSshUrl']
+                    else:
+                        repoPath = repo['sGitUrl']
+                else:
+                    sys.exit('Unknown VCS type for repo: ' + localPath) 
+                
                 if repo['sStatus'] != 'deleted':
-                    ourRepos.append({'repoPath': repoPath, 'project': project['sName'], 
-                        'repoGroup': repoGroup['sName'], 'repo': repo['sName']})
+                    ourRepos.append({'repoPath': repoPath, 'localPath': localPath, 'project': project['sName'], 
+                        'repoGroup': repoGroup['sName'], 'repo': repo['sName'], 'vcs': repo['vcs']})
 
     if verbose:
-        print console_encode('Found %d repositories' % len(ourRepos))
+        print console_encode('Found %d repositories (%d Hg, %d Git)' % (len(ourRepos), numHgRepos, numGitRepos))
 
-    return sorted(ourRepos, key=itemgetter('project', 'repoGroup', 'repo'))
+    return sorted(ourRepos, key=itemgetter('vcs', 'project', 'repoGroup', 'repo'))
 
 
-def backup_repo(clone_url, target_dir, verbose, update):
+def backup_hg_repo(clone_url, target_dir, verbose, update):
     """
-    Backup the specified repository. Returns True if successful. If
+    Backup the specified Hg repository. Returns True if successful. If
     the backup fails, prints an error message and returns False.
     """
     backup_method = 'clone'
@@ -229,13 +244,94 @@ def backup_repo(clone_url, target_dir, verbose, update):
     if proc.returncode:
         print console_encode('**** FAILED ****')
         print console_encode('*' * 60)
-        print console_encode(u'Error backing up repository %s\nError was: %s' %
+        print console_encode(u'Error backing up Hg repository %s\nError was: %s' %
             (clone_url, stderrdata))
         print console_encode('*' * 60)
         return False
 
     if verbose:
-        print console_encode('backed up using method %s' %
+        print console_encode('backed up using Hg method %s' %
+            backup_method.upper())
+
+    return True
+
+
+def backup_git_repo(clone_url, target_dir, verbose, update):
+    """
+    Backup the specified Git repository. Returns True if successful. If
+    the backup fails, prints an error message and returns False.
+    """
+    backup_method = 'clone'
+
+    # If the filesystem does not use Unicode (from Python’s
+    # perspective), convert target_dir to plain ASCII.
+    if not sys.getfilesystemencoding().upper().startswith('UTF'):
+        target_dir = target_dir.encode('ascii', 'xmlcharrefreplace')
+
+    # Does the target directory already exist?
+    if os.path.isdir(target_dir):
+        # Yes, it exists. Does it refer to the same repository?
+        default = Popen(['git', '-C', target_dir, 'config', '--get', 'remote.origin.url'],
+            stdout=PIPE, stderr=PIPE).communicate()[0].strip()
+        default = urllib2.unquote(default)
+        
+        if default == clone_url:
+            # It exists and is identical. We will pull.
+            backup_method = 'pull'
+        else:
+            # It exists but refers to a different repo or is not a
+            # repo. Move it to an archive directory.
+            (parent_dir, repo_name) = os.path.split(target_dir)
+            if verbose:
+                print console_encode('exists but is not the same repo'),
+                sys.stdout.flush()
+            new_name = 'archive/%f-%s' % (time.time(), repo_name)
+            new_dir = os.path.join(parent_dir, new_name)
+            os.renames(target_dir, new_dir)
+            if verbose:
+                print console_encode('(archived); continuing backup...'),
+                sys.stdout.flush()
+    else:
+        # Path doesn’t exist: create it
+        os.makedirs(target_dir)
+
+    # Back it up
+    if backup_method == 'clone':
+        if update:
+            args = ['git', 'clone', clone_url, target_dir]
+        else:
+            args = ['git', 'clone', '--no-checkout', clone_url, target_dir]
+    else:
+        if update:
+            args = ['git', '-C', target_dir, 'pull', target_dir]
+        else:
+            args = ['git', '-C', target_dir, 'fetch', target_dir]
+
+    if debug:
+        print "Backup Method:", backup_method
+        print "args:", args
+
+    # KilnAuth uses os.path.expanduser which should use
+    # %USERPROFILE%. When run from Scheduled Tasks, it does not seem
+    # to work unless you also set %HOMEDRIVE% and %HOMEPATH%.
+    child_env = os.environ
+    if os.name == 'nt':
+        (drive, path) = os.path.splitdrive(os.environ['USERPROFILE'])
+        child_env['HOMEDRIVE'] = drive
+        child_env['HOMEPATH'] = path
+
+    proc = Popen(args, stdout=PIPE, stderr=PIPE, env=child_env)
+    (_, stderrdata) = proc.communicate()
+    if proc.returncode:
+        print console_encode('**** FAILED ****')
+        print console_encode('*' * 60)
+        print console_encode(u'Error backing up Git repository %s\nError was: %s' %
+            (clone_url, stderrdata))
+        print console_encode('*' * 60)
+        return False
+
+    if verbose:
+        print console_encode('backed up using Git method %s' %
             backup_method.upper())
 
     return True
@@ -297,8 +393,7 @@ def main():
 
     # Save configuration
     configfile = open(os.path.join(destination_dir, CONFIG_FILE), 'w+')
-    config = {'server': options.server, 'token': options.token, 
-        'scheme': options.scheme}
+    config = {'server': options.server, 'token': options.token}
     json.dump(config, configfile, indent=4)
     configfile.write('\n')
     configfile.close()
@@ -312,13 +407,10 @@ def main():
     # Keep track of overall success status. We continue backing up
     # even if there’s an error.
     overall_success = True
-
-    kiln_path = 'kiln/'
-    if ('kilnhg.com' in options.server.lower()):
-        kiln_path = ''
         
     # Back up the repositories
-    repos = get_repos(options.scheme, options.server, kiln_path, options.token, options.verbose)
+    repos = get_repos(options.server, options.token, options.ssh, options.verbose)
+
     if debug:
         print "Server:", options.server
         print "token:", options.token
@@ -336,7 +428,7 @@ def main():
         # Filter. Case-insensitive. (Kiln won’t let you create two
         # groups or projects with the same name but different case.)
         repos = [_ for _ in repos if
-            _['repoPath'].lower().startswith(limit.lower())]
+            _['localPath'].lower().startswith(limit.lower())]
 
         if options.verbose:
             if len(repos) == 0:
@@ -359,9 +451,9 @@ def main():
         # Convert the repo path for subdirectory use if necessary
         # on Windows (/ to \).
         if os.sep == '/':
-            subdirectory = repo['repoPath']
+            subdirectory = repo['localPath']
         else:
-            subdirectory = os.path.normpath(repo['repoPath'])
+            subdirectory = os.path.normpath(repo['localPath'])
 
         if options.verbose:
             # For the progress message, show the project and group
@@ -392,7 +484,7 @@ def main():
 
             sys.stdout.flush()
 
-        clone_url = encode_url('%s://%s/%sCode/%s' % (options.scheme, options.server, kiln_path, repo['repoPath']))
+        clone_url = encode_url(repo['repoPath'])
         target_dir = unicode(os.path.join(destination_dir, subdirectory))
 
         if debug:
@@ -403,7 +495,10 @@ def main():
             print "options.verbose:", options.verbose
             print "options.update:", options.update
 
-        success = backup_repo(clone_url, target_dir, options.verbose, options.update)
+        if repo['vcs'] == 1:
+            success = backup_hg_repo(clone_url, target_dir, options.verbose, options.update)
+        elif repo['vcs'] == 2:
+            success = backup_git_repo(clone_url, target_dir, options.verbose, options.update)
         overall_success = overall_success and success
 
     if overall_success:
